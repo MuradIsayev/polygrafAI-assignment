cce46791e3eb508b3d1dc2ac892af49bde23b018
MurAd <muradisayevazer@gmail.com>
2023-12-02 19:19:38 +0400

    APIs moved to auth folder

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,14 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { GetUser } from '../auth/decorators/get-user.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get(':email')
-  findOne(@Param('email') email: string) {
-    return this.usersService.findOneByEmail(email);
-  }
-
-  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
   getUserInfo(@GetUser() user: User) {
     return this.usersService.getUserInfo(user);
   }
